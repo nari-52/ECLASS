3b6ce0a70fd0b8e26431421d1bf494d93d4d99ff
nari-52 <60787793+nari-52@users.noreply.github.com>
2020-08-10 09:29:55 +0900

    아이디찾기

--- Eclass/src/main/java/com/spring/nari/service/InterMemberService.java
+++ Eclass/src/main/java/com/spring/nari/service/InterMemberService.java
@@ -8,4 +8,12 @@ public interface InterMemberService {
 
 	MemberVO getLoginMember(HashMap<String, String> paraMap); // 로그인 처리하기
 
+	String idDuplicateCheck(String userid); // AJAX를 이용하여 회원가입 아이디 중복검사
+
+	int registerMember(MemberVO mvo); // 회원가입 하기
+
+	String mobileDuplicateCheck(String mobile); // AJAX를 이용하여 휴대전화 중복검사
+
+	String idFind(HashMap<String, String> paraMap); // 아이디 찾기
+
 }
